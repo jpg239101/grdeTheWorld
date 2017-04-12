586d7007a399ffbd1225fdf6aad7dbfed876ae0e
jg101 <jpg239101@gmail.com>
2017-04-12 14:28:58 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        Scanner sc1 = new Scanner(new File("rawDataU6.txt"));
+        sc1.nextLine();
+
+
+
+    }
+}
